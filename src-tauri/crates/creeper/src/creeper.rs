aef607ac037e031f7b031017f5681e1d1d8ee949
liwuhou <hugewilliam@foxmail.com>
2024-01-15 09:55:49 +0800

    temp

--- src-tauri/crates/creeper/src/creeper.rs
+++ src-tauri/crates/creeper/src/creeper.rs
@@ -1,20 +1,108 @@
-use std::error::Error;
+extern crate dirs;
+
+use anyhow::Result;
+use select::document::Document;
+use select::node::Node;
+use select::predicate::{Class, Name};
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::collections::HashMap;
+use std::fs::{self, metadata, File};
+use std::io::Write;
+use std::path::PathBuf;
 
 const FETCH_URL: &str = "https://en.wikipedia.org/wiki/List_of_countries_by_life_expectancy";
 
-pub fn fetch() -> Result<String, Box<dyn Error>> {
-    let resp = reqwest::blocking::get(FETCH_URL)?.text()?;
-
-    Ok(resp)
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CountryInfo {
+    pub all: f32,
+    pub male: f32,
+    pub female: f32,
 }
 
-#[cfg(test)]
-mod test {
-    use super::*;
+fn ensure_tmp_exist() -> Result<()> {
+    if let Some(tmp_path) = get_tmp_file_path().parent() {
+        fs::create_dir_all(tmp_path)?;
+    }
+    Ok(())
+}
 
-    fn it_works() -> Result<(), Box<dyn Error>> {
-        let resp = fetch()?;
-        println!("{:?}", resp);
-        Ok(())
+pub fn get_tmp_file_path() -> PathBuf {
+    let home_dir = dirs::home_dir().or(Some(PathBuf::from("."))).unwrap();
+
+    home_dir
+        .join(".config")
+        .join("live_progress")
+        .join(".tmp_expectancy.json")
+}
+
+fn set_tmp_file_path(content: &HashMap<String, CountryInfo>) -> Result<()> {
+    ensure_tmp_exist()?;
+    let mut output = File::create(get_tmp_file_path())?;
+    output.write_all(serde_json::to_string(content)?.as_bytes())?;
+
+    Ok(())
+}
+
+fn has_cache() -> Result<bool> {
+    if let Ok(metadata) = metadata(get_tmp_file_path()) {
+        return Ok(metadata.is_file());
+    }
+
+    Ok(false)
+}
+
+pub fn fetch_data() -> Result<HashMap<String, CountryInfo>> {
+    match has_cache() {
+        Ok(true) => {
+            let json = fs::read_to_string(get_tmp_file_path())?;
+            Ok(serde_json::from_str::<HashMap<String, CountryInfo>>(&json)?)
+        }
+        _ => {
+            if let Ok(hashmap) = fetch() {
+                set_tmp_file_path(&hashmap)?;
+                Ok(hashmap)
+            } else {
+                println!("error");
+                // Network error, use default expectancy data
+                Ok(receive_default_expectancy()?)
+            }
+        }
+    }
+}
+
+fn receive_default_expectancy() -> Result<HashMap<String, CountryInfo>> {
+    let json = fs::read_to_string("./default_expectancy.json")?;
+    Ok(serde_json::from_str::<HashMap<String, CountryInfo>>(&json)?)
+}
+
+fn fetch() -> Result<HashMap<String, CountryInfo>, String> {
+    return Err("in".into());
+    // let mut result: HashMap<String, CountryInfo> = HashMap::new();
+    // let resp = reqwest::blocking::get(FETCH_URL)?.text()?;
+    // let document = Document::from(resp.as_str());
+    // if let Some(target_table) = document.find(Class("wikitable")).nth(3) {
+    //     let tbody = target_table.find(Name("tbody")).next().unwrap();
+    //     println!("{:?}", tbody);
+
+    //     for tr in tbody.find(Name("tr")).skip(2) {
+    //         let mut tds = tr.find(Name("td")).take(4);
+    //         if let Some(country_name) = extract_country_name(tds.next()) {
+    //             let all = tds.next().unwrap().text().trim().parse::<f32>()?;
+    //             let male = tds.next().unwrap().text().trim().parse::<f32>()?;
+    //             let female = tds.next().unwrap().text().trim().parse::<f32>()?;
+    //             result.insert(country_name, CountryInfo { all, male, female });
+    //         }
+    //     }
+    // }
+
+    // Ok(result)
+}
+
+fn extract_country_name(node: Option<Node>) -> Option<String> {
+    if let Some(node) = node {
+        Some(node.find(Name("a")).next().unwrap().text().to_string())
+    } else {
+        None
     }
 }
--- src-tauri/crates/creeper/src/main.rs
+++ src-tauri/crates/creeper/src/main.rs
@@ -1,7 +1,7 @@
 mod creeper;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let res = creeper::fetch()?;
-    println!("{}", res);
+    let res = creeper::fetch_data()?;
+    // println!("{:?}", res);
     Ok(())
 }
